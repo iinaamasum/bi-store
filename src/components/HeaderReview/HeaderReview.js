ee5fe0cf020c051bcad9edb4d87d31217806373e
iinaamasum <iinaamasum@gmail.com>
2022-04-04 01:15:19 +0600

    userData and graphData json files are added

---
+++ src/components/HeaderReview/HeaderReview.js
@@ -0,0 +1,13 @@
+import React from 'react';
+
+const HeaderReview = () => {
+  return (
+    <div className="container mx-auto mt-8">
+      <h1 className="text-5xl text-center text-blue-700 font-mono tracking-wide font-bold">
+        Customer Review
+      </h1>
+    </div>
+  );
+};
+
+export default HeaderReview;
--- src/components/Home/Home.js
+++ src/components/Home/Home.js
@@ -1,35 +1,12 @@
-import React from 'react';
-import { MdArrowForwardIos } from 'react-icons/md';
-import logo from '../../Assets/home.jpg';
+import Header from '../Header/Header';
+import HeaderReview from '../HeaderReview/HeaderReview';
 import './Home.css';
 
 const Home = () => {
   return (
-    <div className="md:flex items-center justify-between container__home pt-3">
-      <div className="md:w-1/2 pr-8 order-last md:order-first">
-        <h1 className="text-4xl lg:text-6xl 2xl:text-8xl text-red-500 font-mono font-bold">
-          The bi-cycle <br /> You want
-        </h1>
-        <h2 className="text-3xl md:text-5xl font-serif text-blue-500 font-semibold pt-8">
-          Your Dream lies here
-        </h2>
-
-        <p className="lg:text-xl pt-4 text-justify">
-          Hi there. If you want to buy a bi-cycle this the best store for you.
-          You can compare among hundreds of bi-cycle and choose the right one
-          for you. See our review to know more.
-        </p>
-
-        <button className="border-2 border-gray-700 rounded-lg mt-3 px-10 py-4 hover:bg-gray-700 hover:text-white transition-all duration-500 ease-in-out">
-          <span className="text-2xl tracking-wide font-mono font-semibold inline-flex items-center">
-            <span className="pt-1">Live Show</span>
-            <MdArrowForwardIos className="ml-4" size={30} />
-          </span>
-        </button>
-      </div>
-      <div className="md:w-1/2 home__img rounded bg-gray-400">
-        <img className="rounded " src={logo} alt="" srcSet="" />
-      </div>
+    <div>
+      <Header />
+      <HeaderReview />
     </div>
   );
 };
